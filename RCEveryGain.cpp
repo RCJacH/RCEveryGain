7beaf40e5a88e39e57d23ce3fe80f8458673a985
RCJacH <RCJacH@outlook.com>
2024-07-01 22:12:57 +0800

    add irect variable for all sections

--- RCEveryGain.cpp
+++ RCEveryGain.cpp
@@ -1,9 +1,9 @@
 #include "RCEveryGain.h"
-#include "IPlug_include_in_plug_src.h"
 #include "IControls.h"
+#include "IPlug_include_in_plug_src.h"
 
 RCEveryGain::RCEveryGain(const InstanceInfo& info)
-: iplug::Plugin(info, MakeConfig(kNumParams, kNumPresets))
+  : iplug::Plugin(info, MakeConfig(kNumParams, kNumPresets))
 {
   fader_amp = 0.0;
 
@@ -25,23 +25,31 @@ RCEveryGain::RCEveryGain(const InstanceInfo& info)
   GetParam(kTrimSide)->InitDouble("Side Trim", 0., -6., 6.0, 0.000001, "dB");
 
 #if IPLUG_EDITOR // http://bit.ly/2S64BDd
-  mMakeGraphicsFunc = [&]() {
-    return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS, GetScaleForScreen(PLUG_WIDTH, PLUG_HEIGHT));
-  };
-  
+  mMakeGraphicsFunc = [&]() { return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS, GetScaleForScreen(PLUG_WIDTH, PLUG_HEIGHT)); };
+
   mLayoutFunc = [&](IGraphics* pGraphics) {
     pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
     pGraphics->AttachPanelBackground(IColor::FromHSLA(0.5972, 0.12, 0.3));
     pGraphics->LoadFont("Roboto-Regular", ROBOTO_FN);
     const IRECT b = pGraphics->GetBounds();
-		pGraphics->AttachControl(new ITextControl(IRECT(100.0, 0.0, 700.0, 54.0), "RCEveryGain", IText(40.0, COLOR_WHITE), COLOR_TRANSPARENT));
-		pGraphics->AttachControl(new ITextControl(IRECT(100.0, 56.0, 124.0, 72.0), "S", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
-		pGraphics->AttachControl(new ITextControl(IRECT(100.0, 72.0, 124.0, 88.0), "H", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
-		pGraphics->AttachControl(new ITextControl(IRECT(100.0, 88.0, 124.0, 104.0), "I", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
-		pGraphics->AttachControl(new ITextControl(IRECT(100.0, 104.0, 124.0, 120.0), "F", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
-		pGraphics->AttachControl(new ITextControl(IRECT(100.0, 120.0, 124.0, 136.0), "T", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
-    //pGraphics->AttachControl(new ITextControl(b.GetMidVPadded(50), "Hello iPlug 2!", IText(50)));
-    // pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetVShifted(-100), kGain));
+    const IRECT center = b.GetMidHPadded(300);
+    const IRECT upper = center.FracRectVertical(0.618 * 0.382, true);
+    const IRECT lower = center.GetReducedFromTop(upper.H());
+    const IRECT shift = upper.FracRectVertical(0.618);
+    const IRECT header = upper.GetReducedFromBottom(shift.H());
+    const IRECT volume = lower.FracRectHorizontal(0.618, true);
+    const IRECT fader = lower.GetReducedFromRight(volume.W());
+    const IRECT gain = volume.FracRectVertical(0.5, true);
+    const IRECT trim = volume.FracRectVertical(0.5);
+
+    pGraphics->AttachControl(new ITextControl(header, "RCEveryGain", IText(40.0, COLOR_WHITE), COLOR_TRANSPARENT));
+    pGraphics->AttachControl(new ITextControl(IRECT(100.0, 56.0, 124.0, 72.0), "S", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
+    pGraphics->AttachControl(new ITextControl(IRECT(100.0, 72.0, 124.0, 88.0), "H", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
+    pGraphics->AttachControl(new ITextControl(IRECT(100.0, 88.0, 124.0, 104.0), "I", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
+    pGraphics->AttachControl(new ITextControl(IRECT(100.0, 104.0, 124.0, 120.0), "F", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
+    pGraphics->AttachControl(new ITextControl(IRECT(100.0, 120.0, 124.0, 136.0), "T", IText(20.0, COLOR_WHITE), COLOR_TRANSPARENT));
+    // pGraphics->AttachControl(new ITextControl(b.GetMidVPadded(50), "Hello iPlug 2!", IText(50)));
+    //  pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetVShifted(-100), kGain));
   };
 #endif
 }
@@ -59,12 +67,12 @@ void RCEveryGain::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
   const double trim_master = GetParam(kTrimMaster)->Value();
   const double gain_left = GetParam(kGainLeft)->Value();
   const double trim_left = GetParam(kTrimLeft)->Value();
-  const double gain_right= GetParam(kGainRight)->Value();
+  const double gain_right = GetParam(kGainRight)->Value();
   const double trim_right = GetParam(kTrimRight)->Value();
-  const double gain_mid= GetParam(kGainMid)->Value();
-  const double trim_mid= GetParam(kTrimMid)->Value();
-  const double gain_side= GetParam(kGainSide)->Value();
-  const double trim_side= GetParam(kTrimSide)->Value();
+  const double gain_mid = GetParam(kGainMid)->Value();
+  const double trim_mid = GetParam(kTrimMid)->Value();
+  const double gain_side = GetParam(kGainSide)->Value();
+  const double trim_side = GetParam(kTrimSide)->Value();
 
   double shift_amp = std::pow(2.0, (macro_shift + micro_shift) * shift_size);
   double fader_smoothing_inv = 1.0 / (fader_smoothing + 1.0);
@@ -76,8 +84,9 @@ void RCEveryGain::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
   double side_amp = DBToAmp(gain_side) * DBToAmp(trim_side);
 
   const int nChans = NOutChansConnected();
-  
-  for (int s = 0; s < nFrames; s++) {
+
+  for (int s = 0; s < nFrames; s++)
+  {
     fader_amp = ((fader_amp * fader_smoothing) + fader_target) * fader_smoothing_inv;
     double gain = shift_amp * fader_amp * master_amp;
 
