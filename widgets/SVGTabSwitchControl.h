b00d597b5fc3873eb30f140750abeb34a8f37a04
RCJacH <RCJacH@outlook.com>
2024-07-20 06:53:36 +0800

    add support for text offset

--- widgets/SVGTabSwitchControl.h
+++ widgets/SVGTabSwitchControl.h
@@ -8,26 +8,6 @@ BEGIN_IGRAPHICS_NAMESPACE
 class SVGTabSwitchControl : public IVTabSwitchControl
 {
 public:
-  /** Constructs a vector tab switch control, linked to a parameter
-   * @param bounds The control's bounds
-   * @param paramIdx The parameter index to link this control to
-   * @param offImage An SVG for the off state of the button
-   * @param onImage An SVG for the on state of the button
-   * @param options An initializer list of CStrings for the button labels to override the parameter display text labels. Supply an empty {} list if you don't want to do that.
-   * @param label The IVControl label CString
-   * @param gap Distance between each button
-   * @param style The styling of this vector control \see IVStyle
-   * @param direction The direction of the buttons */
-  SVGTabSwitchControl(const IRECT& bounds,
-                      int paramIdx,
-                      const ISVG& offImage,
-                      const ISVG& onImage,
-                      const std::vector<const char*>& options = {},
-                      const char* label = "",
-                      const float gap = 0.f,
-                      const IVStyle& style = DEFAULT_STYLE,
-                      EDirection direction = EDirection::Horizontal);
-
   /** Constructs a vector tab switch control, with an action function (no parameter)
    * @param bounds The control's bounds
    * @param aF An action function to execute when a button is clicked \see IActionFunction
@@ -37,6 +17,7 @@ public:
    * @param label The IVControl label CString
    * @param gap Distance between each button
    * @param style The styling of this vector control \see IVStyle
+   * @param textOffset The styling of this vector control \see IVStyle
    * @param direction The direction of the buttons */
   SVGTabSwitchControl(const IRECT& bounds,
                       IActionFunction aF,
@@ -46,6 +27,29 @@ public:
                       const char* label = "",
                       const float gap = 0.f,
                       const IVStyle& style = DEFAULT_STYLE,
+                      const std::array<float, 4> textOffsets = defaultTextOffsets,
+                      EDirection direction = EDirection::Horizontal);
+
+  /** Constructs a vector tab switch control, linked to a parameter
+   * @param bounds The control's bounds
+   * @param paramIdx The parameter index to link this control to
+   * @param offImage An SVG for the off state of the button
+   * @param onImage An SVG for the on state of the button
+   * @param options An initializer list of CStrings for the button labels to override the parameter display text labels. Supply an empty {} list if you don't want to do that.
+   * @param label The IVControl label CString
+   * @param gap Distance between each button
+   * @param style The styling of this vector control \see IVStyle
+   * @param textOffset The styling of this vector control \see IVStyle
+   * @param direction The direction of the buttons */
+  SVGTabSwitchControl(const IRECT& bounds,
+                      int paramIdx,
+                      const ISVG& offImage,
+                      const ISVG& onImage,
+                      const std::vector<const char*>& options = {},
+                      const char* label = "",
+                      const float gap = 0.f,
+                      const IVStyle& style = DEFAULT_STYLE,
+                      const std::array<float, 4> textOffsets = defaultTextOffsets,
                       EDirection direction = EDirection::Horizontal);
 
   /** Constructs an SVG button control, with an action function and a single image, with color overrides
@@ -58,6 +62,7 @@ public:
    * @param label The IVControl label CString
    * @param gap Distance between each button
    * @param style The styling of this vector control \see IVStyle
+   * @param textOffset The styling of this vector control \see IVStyle
    * @param direction The direction of the buttons */
   SVGTabSwitchControl(const IRECT& bounds,
                       IActionFunction aF,
@@ -68,6 +73,7 @@ public:
                       const char* label = "",
                       const float gap = 0.f,
                       const IVStyle& style = DEFAULT_STYLE,
+                      const std::array<float, 4> textOffsets = defaultTextOffsets,
                       EDirection direction = EDirection::Horizontal);
 
   /** Constructs an SVG button control, with an action function and a single image, with color overrides
@@ -80,6 +86,7 @@ public:
    * @param label The IVControl label CString
    * @param gap Distance between each button
    * @param style The styling of this vector control \see IVStyle
+   * @param textOffset The styling of this vector control \see IVStyle
    * @param direction The direction of the buttons */
   SVGTabSwitchControl(const IRECT& bounds,
                       int paramIdx,
@@ -90,28 +97,43 @@ public:
                       const char* label = "",
                       const float gap = 0.f,
                       const IVStyle& style = DEFAULT_STYLE,
+                      const std::array<float, 4> textOffsets = defaultTextOffsets,
                       EDirection direction = EDirection::Horizontal);
 
   virtual ~SVGTabSwitchControl() { mTabLabels.Empty(true); }
 
   virtual void DrawButton(IGraphics& g, const IRECT& bounds, bool pressed, bool mouseOver, ETabSegment segment, bool disabled) override;
+  virtual void DrawButtonText(IGraphics& g, const IRECT& bounds, bool pressed, bool mouseOver, ETabSegment segment, bool disabled, const char* textStr) override;
 
   void OnResize() override;
+
+private:
+  static constexpr std::array<float, 4> defaultTextOffsets = {0.f, 0.f, 0.f, 0.f};
 
 protected:
   ISVG mOffSVG;
   ISVG mOnSVG;
   float mGap;
+  std::array<float, 4> mTextOffset;
   std::array<IColor, 4> mColors;
   EColorReplacement mColorReplacement = EColorReplacement::None;
 };
 
-SVGTabSwitchControl::SVGTabSwitchControl(
-  const IRECT& bounds, IActionFunction aF, const ISVG& offImage, const ISVG& onImage, const std::vector<const char*>& options, const char* label, float gap, const IVStyle& style, EDirection direction)
+SVGTabSwitchControl::SVGTabSwitchControl(const IRECT& bounds,
+                                         IActionFunction aF,
+                                         const ISVG& offImage,
+                                         const ISVG& onImage,
+                                         const std::vector<const char*>& options,
+                                         const char* label,
+                                         float gap,
+                                         const IVStyle& style,
+                                         const std::array<float, 4> textOffsets,
+                                         EDirection direction)
   : IVTabSwitchControl(bounds, kNoParameter, options, label, style, EVShape::Rectangle, direction)
   , mOffSVG(offImage)
   , mOnSVG(onImage)
   , mGap(gap)
+  , mTextOffset(textOffsets)
 {
   AttachIControl(this, label);
   mText = style.valueText;
@@ -124,12 +146,21 @@ SVGTabSwitchControl::SVGTabSwitchControl(
   }
 }
 
-SVGTabSwitchControl::SVGTabSwitchControl(
-  const IRECT& bounds, int paramIdx, const ISVG& offImage, const ISVG& onImage, const std::vector<const char*>& options, const char* label, float gap, const IVStyle& style, EDirection direction)
+SVGTabSwitchControl::SVGTabSwitchControl(const IRECT& bounds,
+                                         int paramIdx,
+                                         const ISVG& offImage,
+                                         const ISVG& onImage,
+                                         const std::vector<const char*>& options,
+                                         const char* label,
+                                         float gap,
+                                         const IVStyle& style,
+                                         const std::array<float, 4> textOffsets,
+                                         EDirection direction)
   : IVTabSwitchControl(bounds, paramIdx, options, label, style, EVShape::Rectangle, direction)
   , mOffSVG(offImage)
   , mOnSVG(onImage)
   , mGap(gap)
+  , mTextOffset(textOffsets)
 {
   AttachIControl(this, label);
   mText = style.valueText;
@@ -151,6 +182,7 @@ SVGTabSwitchControl::SVGTabSwitchControl(const IRECT& bounds,
                                          const char* label,
                                          float gap,
                                          const IVStyle& style,
+                                         const std::array<float, 4> textOffsets,
                                          EDirection direction)
   : IVTabSwitchControl(bounds, aF, options, label, style, EVShape::Rectangle, direction)
   , mOffSVG(image)
@@ -158,6 +190,7 @@ SVGTabSwitchControl::SVGTabSwitchControl(const IRECT& bounds,
   , mColors(colors)
   , mColorReplacement(colorReplacement)
   , mGap(gap)
+  , mTextOffset(textOffsets)
 {
   AttachIControl(this, label);
   mText = style.valueText;
@@ -179,6 +212,7 @@ SVGTabSwitchControl::SVGTabSwitchControl(const IRECT& bounds,
                                          const char* label,
                                          float gap,
                                          const IVStyle& style,
+                                         const std::array<float, 4> textOffsets,
                                          EDirection direction)
   : IVTabSwitchControl(bounds, paramIdx, options, label, style, EVShape::Rectangle, direction)
   , mOffSVG(image)
@@ -186,6 +220,7 @@ SVGTabSwitchControl::SVGTabSwitchControl(const IRECT& bounds,
   , mColors(colors)
   , mColorReplacement(colorReplacement)
   , mGap(gap)
+  , mTextOffset(textOffsets)
 {
   AttachIControl(this, label);
   mText = style.valueText;
@@ -223,6 +258,16 @@ void SVGTabSwitchControl::DrawButton(IGraphics& g, const IRECT& r, bool pressed,
     g.DrawStretchedSVG(mOnSVG, r, &mBlend, pOnColorStroke, pOnColorFill);
   else
     g.DrawStretchedSVG(mOffSVG, r, &mBlend, pOffColorStroke, pOffColorFill);
+}
+
+void SVGTabSwitchControl::DrawButtonText(IGraphics& g, const IRECT& r, bool pressed, bool mouseOver, ETabSegment segment, bool disabled, const char* textStr)
+{
+  if (CStringHasContents(textStr))
+  {
+    float h = pressed ? mTextOffset[2] : mTextOffset[0];
+    float v = pressed ? mTextOffset[3] : mTextOffset[1];
+    g.DrawText(mStyle.valueText, textStr, r.GetOffset(h < 0.f ? h : 0.f, v < 0.f ? v : 0.f, h > 0.f ? -h : 0.f, v > 0.f ? -v : 0.f), &mBlend);
+  }
 }
 
 void SVGTabSwitchControl::OnResize()
