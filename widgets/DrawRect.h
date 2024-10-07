c78adcbc923ca1b1b98fed04b14867d853e392c3
RCJacH <RCJacH@outlook.com>
2024-10-07 23:15:26 +0800

    refactor RCSlider to use RCSliderControl

--- widgets/DrawRect.h
+++ widgets/DrawRect.h
@@ -8,7 +8,7 @@
 BEGIN_IPLUG_NAMESPACE
 BEGIN_IGRAPHICS_NAMESPACE
 
-class DrawRect : public IVectorBase
+class DrawRect
 {
 public:
   enum ControlState
@@ -28,12 +28,9 @@ public:
     Filled,
   };
 
-  DrawRect(const Color::HSLA color, const IVStyle& style);
+  DrawRect(const Color::HSLA color);
 
   virtual ~DrawRect() {}
-
-  virtual void DrawTrack(IGraphics& g, const IRECT& rect, ControlState state, double pct, DirectionType direction);
-  virtual void DrawValue(IGraphics& g, const IRECT& rect, ControlState state, DirectionType direction);
 
   EDirection ToEDirection(DirectionType dir);
 
@@ -69,56 +66,13 @@ EDirection DrawRect::ToEDirection(DirectionType dir)
   case DirectionType::VerticalSplit:
     return EDirection::Vertical;
   }
+  return EDirection::Horizontal;
 };
 
-DrawRect::DrawRect(const Color::HSLA color, const IVStyle& style)
-  : IVectorBase(style, false, true)
-  , mColors(WidgetColors(color))
+DrawRect::DrawRect(const Color::HSLA color)
+  : mColors(WidgetColors(color))
   , mHoverColors(mColors.HoverColors())
   , mPressColors(mColors.PressColors()) {};
-
-void DrawRect::DrawTrack(IGraphics& g, const IRECT& rect, ControlState state = ControlState::kNormal, double pct = 1., DirectionType direction = DirectionType::Horizontal)
-{
-  const float border_width = mStyle.drawFrame ? mStyle.frameThickness : 0.f;
-  const IRECT contentBounds = rect.GetPadded(-border_width);
-  WidgetColors color = GetColors(state);
-  IBlend blend = mControl->GetBlend();
-  if (border_width)
-    g.DrawRect(color.GetBorderColor(), rect, &blend);
-
-
-  IRECT valueBounds;
-  switch (direction)
-  {
-  case DirectionType::Horizontal:
-  case DirectionType::Vertical:
-    const EDirection fracDirection = (direction == DirectionType::Horizontal) ? EDirection::Horizontal : EDirection::Vertical;
-    valueBounds = contentBounds.FracRect(fracDirection, pct);
-    break;
-  case DirectionType::HorizontalSplit:
-  case DirectionType::VerticalSplit:
-    const EDirection fracDirection = (direction == DirectionType::HorizontalSplit) ? EDirection::Horizontal : EDirection::Vertical;
-    valueBounds = contentBounds.FracRect(fracDirection, 0.5, pct > .5).FracRect(fracDirection, pct - .5, pct < .5);
-    break;
-  case DirectionType::Filled:
-    valueBounds = contentBounds;
-    break;
-  }
-  g.FillRect(color.GetColor(), valueBounds, &blend);
-};
-
-void DrawRect::DrawValue(IGraphics& g, const IRECT& rect, ControlState state, DirectionType direction)
-{
-  if (!mStyle.showValue)
-    return;
-
-  WidgetColors color = GetColors(state);
-
-  IRECT valueBounds = rect;
-  IBlend blend = mControl->GetBlend();
-  mStyle.valueText.mFGColor = color.GetLabelColor();
-  g.DrawText(mStyle.valueText, mValueStr.Get(), mValueBounds, &blend);
-};
 
 
 END_IGRAPHICS_NAMESPACE
--- widgets/RCSlider.h
+++ widgets/RCSlider.h
@@ -4,156 +4,106 @@
 #include "IGraphicsStructs.h"
 #include "Widgets/Color.h"
 #include "Widgets/DrawRect.h"
+#include "widgets/RCSliderControl.h"
 #include "widgets/WidgetColors.h"
 
 BEGIN_IPLUG_NAMESPACE
 BEGIN_IGRAPHICS_NAMESPACE
 
-class RCSlider : public ISliderControlBase, public DrawRect
+class RCSlider : public RCSliderControl, public IVectorBase, public DrawRect
 {
 public:
   RCSlider(const IRECT& bounds,
            int paramIdx = kNoParameter,
-           const Color::HSLA& color,
+           const Color::HSLA& color = Color::HSLA{},
            const IVStyle& style = DEFAULT_STYLE,
            bool valueIsEditable = false,
            DirectionType dir = DirectionType::Horizontal,
-           double gearing = DEFAULT_GEARING);
+           double gearing = 1.0);
 
   RCSlider(const IRECT& bounds,
            IActionFunction aF,
-           const Color::HSLA& color,
+           const Color::HSLA& color = Color::HSLA{},
            const IVStyle& style = DEFAULT_STYLE,
            bool valueIsEditable = false,
            DirectionType dir = DirectionType::Horizontal,
-           double gearing = DEFAULT_GEARING);
+           double gearing = 1.0);
 
   virtual ~RCSlider() {}
 
   virtual void Draw(IGraphics& g);
+  virtual void DrawTrack(IGraphics& g, ControlState state);
+  virtual void DrawValue(IGraphics& g, ControlState state);
 
   void SetActive(const bool active) { mActive = active; };
-  void OnMouseDown(float x, float y, const IMouseMod& mod) override;
-  void OnMouseDblClick(float x, float y, const IMouseMod& mod) override;
-  void OnMouseUp(float x, float y, const IMouseMod& mod) override;
-  void OnMouseOver(float x, float y, const IMouseMod& mod) override;
-  void OnMouseOut() override
-  {
-    mValueMouseOver = false;
-    ISliderControlBase::OnMouseOut();
-  }
-  bool IsHit(float x, float y) const override;
-  void OnResize() override;
-  void SetDirty(bool push, int valIdx = kNoValIdx) override;
-  void OnInit() override;
 
 protected:
-  IRECT mRECT = IRECT();
-  DirectionType mDirection;
+  DirectionType mDirectionType;
   bool mActive = false;
-  bool mValueMouseOver = false;
 };
 
-RCSlider::RCSlider(const IRECT& bounds, int paramIdx, const Color::HSLA& color, const IVStyle& style, bool valueIsEditable = false, DirectionType dir, double gearing)
-  : ISliderControlBase(bounds, paramIdx, DrawRect::ToEDirection(dir), gearing)
-  , DrawRect(color, style)
-  , mDirection(dir)
+RCSlider::RCSlider(const IRECT& bounds, int paramIdx, const Color::HSLA& color, const IVStyle& style, bool valueIsEditable, DirectionType dir, double gearing)
+  : RCSliderControl(bounds, paramIdx, DrawRect::ToEDirection(dir), gearing)
+  , IVectorBase(style)
+  , DrawRect(color)
+  , mDirectionType(dir)
 {
-  DisablePrompt(!valueIsEditable);
-  mText = style.valueText;
-  mHideCursorOnDrag = style.hideCursor;
-  mShape = EVShape::Rectangle;
 }
 
 RCSlider::RCSlider(const IRECT& bounds, IActionFunction aF, const Color::HSLA& color, const IVStyle& style, bool valueIsEditable, DirectionType dir, double gearing)
-  : ISliderControlBase(bounds, aF, DrawRect::ToEDirection(dir), gearing)
-  , DrawRect(color, style)
-  , mDirection(dir)
+  : RCSliderControl(bounds, aF, DrawRect::ToEDirection(dir), gearing)
+  , IVectorBase(style)
+  , DrawRect(color)
+  , mDirectionType(dir)
 {
-  DisablePrompt(!valueIsEditable);
-  mText = style.valueText;
-  mHideCursorOnDrag = style.hideCursor;
-  mShape = EVShape::Rectangle;
-};
+}
 
 void RCSlider::Draw(IGraphics& g)
 {
-  ControlState state = mMouseDown ? ControlState::kPressed : mMouseIsOver ? ControlState::kHovered : ControlState::kNormal;
-  DrawTrack(g, mRECT, state, GetValue(), mDirection);
-  DrawValue(g, mRECT, state, mDirection);
-};
+  ControlState state = mMouseControl.IsAnyPressing() ? ControlState::kPressed : mMouseControl.IsHovering() ? ControlState::kHovered : ControlState::kNormal;
+  DrawTrack(g, state);
+  DrawValue(g, state);
+}
 
-void RCSlider::OnMouseDown(float x, float y, const IMouseMod& mod)
+void RCSlider::DrawTrack(IGraphics& g, ControlState state)
 {
-  if (mStyle.showValue && mValueBounds.Contains(x, y))
+  const float border_width = mStyle.drawFrame ? mStyle.frameThickness : 0.f;
+  WidgetColors color = GetColors(state);
+  if (border_width)
+    g.DrawRect(color.GetBorderColor(), mRECT, &mBlend, border_width);
+
+  const IRECT contentBounds = mRECT.GetPadded(-border_width);
+  IRECT valueBounds;
+  EDirection fracDirection;
+  const double pct = GetValue();
+  switch (mDirectionType)
   {
-    PromptUserInput(mValueBounds);
+  case DirectionType::Horizontal:
+  case DirectionType::Vertical:
+    fracDirection = (mDirectionType == DirectionType::Horizontal) ? EDirection::Horizontal : EDirection::Vertical;
+    valueBounds = contentBounds.FracRect(fracDirection, pct);
+    break;
+  case DirectionType::HorizontalSplit:
+  case DirectionType::VerticalSplit:
+    fracDirection = (mDirectionType == DirectionType::HorizontalSplit) ? EDirection::Horizontal : EDirection::Vertical;
+    valueBounds = contentBounds.FracRect(fracDirection, 0.5, pct > .5).FracRect(fracDirection, abs(0.5 - pct) * 2., pct < .5);
+    break;
+  case DirectionType::Filled:
+    valueBounds = contentBounds;
+    break;
   }
-  else
-  {
-    ISliderControlBase::OnMouseDown(x, y, mod);
-  }
+  g.FillRect(color.GetColor(), valueBounds, &mBlend);
 }
 
-void RCSlider::OnMouseDblClick(float x, float y, const IMouseMod& mod)
+void RCSlider::DrawValue(IGraphics& g, ControlState state)
 {
-#ifdef AAX_API
-  PromptUserInput(mValueBounds);
-#else
-  SetValueToDefault(GetValIdxForPos(x, y));
-#endif
-}
+  if (!mStyle.showValue)
+    return;
 
-void RCSlider::OnMouseUp(float x, float y, const IMouseMod& mod)
-{
-  ISliderControlBase::OnMouseUp(x, y, mod);
-  SetDirty(true);
-}
+  WidgetColors color = GetColors(state);
 
-void RCSlider::OnMouseOver(float x, float y, const IMouseMod& mod)
-{
-  if (mStyle.showValue && !mDisablePrompt)
-    mValueMouseOver = mValueBounds.Contains(x, y);
-
-  ISliderControlBase::OnMouseOver(x, y, mod);
-  SetDirty(false);
-}
-
-void RCSlider::OnResize()
-{
-  SetTargetRECT(MakeRects(mRECT));
-  SetDirty(false);
-}
-
-bool RCSlider::IsHit(float x, float y) const
-{
-  if (!mDisablePrompt)
-  {
-    if (mValueBounds.Contains(x, y))
-    {
-      return true;
-    }
-  }
-
-  return mWidgetBounds.Contains(x, y);
-}
-
-void RCSlider::SetDirty(bool push, int valIdx)
-{
-  ISliderControlBase::SetDirty(push);
-
-  const IParam* pParam = GetParam();
-
-  if (pParam)
-    pParam->GetDisplayWithLabel(mValueStr);
-}
-
-void RCSlider::OnInit()
-{
-  const IParam* pParam = GetParam();
-
-  if (pParam)
-    pParam->GetDisplayWithLabel(mValueStr);
+  mStyle.valueText.mFGColor = color.GetLabelColor();
+  g.DrawText(mStyle.valueText, mValueStr.Get(), mRECT, &mBlend);
 }
 
 END_IGRAPHICS_NAMESPACE
