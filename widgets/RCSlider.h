8e2660b6246940a6c776879e215f9dcac7609571
RCJacH <RCJacH@outlook.com>
2024-10-09 13:03:59 +0800

    fix slider border size

--- widgets/RCSlider.h
+++ widgets/RCSlider.h
@@ -10,39 +10,44 @@
 BEGIN_IPLUG_NAMESPACE
 BEGIN_IGRAPHICS_NAMESPACE
 
+const IVStyle RC_DEFAULT_STYLE =
+  DEFAULT_STYLE.WithShowLabel(false).WithShowValue(true).WithDrawFrame(true).WithFrameThickness(2.f).WithValueText(DEFAULT_VALUE_TEXT.WithSize(16.0).WithFGColor(COLOR_BLACK));
+
+
 class RCSlider : public RCSliderControl, public IVectorBase, public DrawRect
 {
 public:
   RCSlider(const IRECT& bounds,
            int paramIdx = kNoParameter,
            const Color::HSLA& color = Color::HSLA{},
-           const IVStyle& style = DEFAULT_STYLE,
-           bool valueIsEditable = false,
            DirectionType dir = DirectionType::Horizontal,
+           const IVStyle& style = RC_DEFAULT_STYLE,
+           bool valueIsEditable = false,
            double gearing = 1.0);
 
   RCSlider(const IRECT& bounds,
            IActionFunction aF,
            const Color::HSLA& color = Color::HSLA{},
-           const IVStyle& style = DEFAULT_STYLE,
-           bool valueIsEditable = false,
            DirectionType dir = DirectionType::Horizontal,
+           const IVStyle& style = RC_DEFAULT_STYLE,
+           bool valueIsEditable = false,
            double gearing = 1.0);
 
   virtual ~RCSlider() {}
 
   virtual void Draw(IGraphics& g);
-  virtual void DrawTrack(IGraphics& g, ControlState state);
-  virtual void DrawValue(IGraphics& g, ControlState state);
+  virtual void DrawTrack(IGraphics& g, WidgetColors color);
+  virtual void DrawValue(IGraphics& g, WidgetColors color);
 
   void SetActive(const bool active) { mActive = active; };
+
 
 protected:
   DirectionType mDirectionType;
   bool mActive = false;
 };
 
-RCSlider::RCSlider(const IRECT& bounds, int paramIdx, const Color::HSLA& color, const IVStyle& style, bool valueIsEditable, DirectionType dir, double gearing)
+RCSlider::RCSlider(const IRECT& bounds, int paramIdx, const Color::HSLA& color, DirectionType dir, const IVStyle& style, bool valueIsEditable, double gearing)
   : RCSliderControl(bounds, paramIdx, DrawRect::ToEDirection(dir), gearing)
   , IVectorBase(style)
   , DrawRect(color)
@@ -50,7 +55,7 @@ RCSlider::RCSlider(const IRECT& bounds, int paramIdx, const Color::HSLA& color, 
 {
 }
 
-RCSlider::RCSlider(const IRECT& bounds, IActionFunction aF, const Color::HSLA& color, const IVStyle& style, bool valueIsEditable, DirectionType dir, double gearing)
+RCSlider::RCSlider(const IRECT& bounds, IActionFunction aF, const Color::HSLA& color, DirectionType dir, const IVStyle& style, bool valueIsEditable, double gearing)
   : RCSliderControl(bounds, aF, DrawRect::ToEDirection(dir), gearing)
   , IVectorBase(style)
   , DrawRect(color)
@@ -61,16 +66,19 @@ RCSlider::RCSlider(const IRECT& bounds, IActionFunction aF, const Color::HSLA& c
 void RCSlider::Draw(IGraphics& g)
 {
   ControlState state = mMouseControl.IsLPressed() ? ControlState::kPressed : mMouseControl.IsHovering() ? ControlState::kHovered : ControlState::kNormal;
-  DrawTrack(g, state);
-  DrawValue(g, state);
+  WidgetColors color = GetColors(state);
+  DrawTrack(g, color);
+  DrawValue(g, color);
 }
 
-void RCSlider::DrawTrack(IGraphics& g, ControlState state)
+void RCSlider::DrawTrack(IGraphics& g, WidgetColors color)
 {
   const float border_width = mStyle.drawFrame ? mStyle.frameThickness : 0.f;
-  WidgetColors color = GetColors(state);
   if (border_width)
-    g.DrawRect(color.GetBorderColor(), mRECT, &mBlend, border_width);
+  {
+    const IRECT borderBounds = mRECT.GetPadded(-(border_width * .5f));
+    g.DrawRect(color.GetBorderColor(), borderBounds, &mBlend, border_width);
+  }
 
   const IRECT contentBounds = mRECT.GetPadded(-border_width);
   IRECT valueBounds;
@@ -95,12 +103,10 @@ void RCSlider::DrawTrack(IGraphics& g, ControlState state)
   g.FillRect(color.GetColor(), valueBounds, &mBlend);
 }
 
-void RCSlider::DrawValue(IGraphics& g, ControlState state)
+void RCSlider::DrawValue(IGraphics& g, WidgetColors color)
 {
   if (!mStyle.showValue)
     return;
-
-  WidgetColors color = GetColors(state);
 
   mStyle.valueText.mFGColor = color.GetLabelColor();
   g.DrawText(mStyle.valueText, mValueStr.Get(), mRECT, &mBlend);
